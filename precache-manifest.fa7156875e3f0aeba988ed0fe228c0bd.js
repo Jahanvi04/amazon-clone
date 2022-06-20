dc57befa55be20e8dded7ec75056fdf68fe5e741
Jahanvi04 <jahanvichaudhary2001@gmail.com>
2022-06-20 12:29:04 +0530

    Updates

---
+++ precache-manifest.fa7156875e3f0aeba988ed0fe228c0bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126f0d5369e631fc5e988d89a3163bd0",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "38cb841dd7026f949a55",
+    "url": "/amazon-clone/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "72050a9b149830fc3847",
+    "url": "/amazon-clone/static/js/2.96f915ee.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/amazon-clone/static/js/2.96f915ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38cb841dd7026f949a55",
+    "url": "/amazon-clone/static/js/main.e1f6a133.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
