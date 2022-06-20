54f9b28dabe7b6866a25773485531750b751893d
Jahanvi04 <70815288+Jahanvi04@users.noreply.github.com>
2022-06-20 12:17:28 +0530

    Delete src directory

--- src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  // The API (cloud function) URL
-  // baseURL: "http://localhost:5001/clone-f13fd/us-central1/api",
-  baseURL: "https://us-central1-clone-f13fd.cloudfunctions.net/api",
-});
-
-export default instance;
