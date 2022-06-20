99509b7f2317b9478c25bc076b869cee5882cbb2
Jahanvi04 <70815288+Jahanvi04@users.noreply.github.com>
2022-06-20 12:25:37 +0530

    Add files via upload

---
+++ build/precache-manifest.32729ded520a9c6ef61c72beb53cad87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14c2d93824f09a2f0d329a0943c6567",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3812b286fb6cc8b8b1e",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "66057293e9aff8f36eaf",
+    "url": "/static/js/2.1152b9cd.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.1152b9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3812b286fb6cc8b8b1e",
+    "url": "/static/js/main.ecea58ac.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
